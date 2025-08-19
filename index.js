b577aabf3966002216ab2c8a0aae6c50bc26be76
guiteixeiram14-del <guiteixeiram14@gmail.com>
2025-08-18 21:03:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import axios from "axios";
 const app = express();
 app.use(express.json());
 
-const INSTANCE_ID = process.env.INSTANCE_ID;
-const TOKEN = process.env.TOKEN;
+const INSTANCE_ID = "3E5EB67EEE35A0C5B10B7A1A2DC5201F";
+const TOKEN = "9876D28DF11799E73AE36B2D";
 
 app.post("/webhook", async (req, res) => {
   const message = req.body?.data?.message;
